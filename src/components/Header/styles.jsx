20e4a2d68c6118ad6c007657a5308eba37f7e6dc
AntoineCau <acauwd@gmail.com>
2021-07-21 13:47:22 +0200

    adding color on header

--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -3,15 +3,15 @@ import styled from 'styled-components';
 const SHeader = styled.div`
   .header {
     width: 100vw;
-    height: 100px;
+    height: 80px;
     position: fixed;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    background-color: #ffff;
-    border: black solid 2px;
+    background-color: #85b8b7;
+    border-bottom: #036770 solid 1px;
     .logo {
-      height: 90px;
+      height: 75px;
     }
     .pages {
       display: flex;
@@ -20,9 +20,17 @@ const SHeader = styled.div`
       li {
         padding: 30px;
         a {
-          color: #85b8b7;
+          border-radius: 3px;
+          padding: 10px;
+          color: #ffff;
           text-decoration: none;
+          font-size: 20px;
           font-family: 'roboto', sans-serif;
+          :hover {
+            transition: 0.5s;
+            color: #85b8b7;
+            background-color: #ffff;
+          }
         }
       }
     }
